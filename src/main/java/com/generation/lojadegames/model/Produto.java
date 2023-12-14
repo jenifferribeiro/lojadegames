cec04642c45ae42295ab44d2f0b5073590606432
jenifferribeiro <js.ribei@hotmail.com>
2023-12-14 12:35:20 -0300

    CRUD Produto

---
+++ src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,85 @@
+package com.generation.lojadegames.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id // Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O Atributo nome é obrigatório!")
+	@Size(min = 5, message = "O Atributo nome deve conter no mínimo 5 caracteres.")
+	private String nome;
+
+	@NotNull(message = "O Atributo descrição é obrigatório!")
+	private String descricao;
+	
+	@NotNull(message = "O Atributo preço é obrigatório!")
+	private Float preco;
+	
+
+	private String foto;
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+	public Float getPreco() {
+		return preco;
+	}
+
+
+	public void setPreco(Float preco) {
+		this.preco = preco;
+	}
+
+
+	public String getFoto() {
+		return foto;
+	}
+
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+	
+
+}
